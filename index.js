1301075c51ad343f05085c2d6c9dbe12de1ad66c
Edward Gao <egao@sent.com>
2017-01-12 21:21:20 -1000

    messaging should work

--- index.js
+++ index.js
@@ -17,13 +17,40 @@ app.get('/api/hello', (req, res) => {
   res.json({ hello });
 });
 
+function clientHandleOp( msg ){
+  let error;
+
+  switch( msg.OP ){
+    case OP.REGISTER:
+      error = `You are already registered as: '${this.username}'`;
+      this.sendOp(OP.ERROR, { error });
+      break;
+    case OP.CHAT:
+      // loop through all players (in the map)
+      //  if the player is not the sender  this.username  !== playerUsername
+      // sendOp(OP.CHAT, { message })
+      players.forEach( (player, playerUsername) => {
+        if(playerUsername !== this.username){
+          let message = msg.payload.message;
+          player.sendOp(OP.CHAT, { username : this.username, message });
+        }
+      });
+      break;
+    default:
+      error = `Unknown OP received. Server does not understand: '${msg.OP}'`;
+      console.warn(error);
+      this.sendOp(OP.ERROR, { error });
+      return;
+  }
+}
+
 function clientReceiveMessage( message ){
   let msg;
   try{
     msg = OP.parse(message);
   }catch(error){
     console.error(error);
-    return this.send(OP.create(OP.ERROR, { error }));
+    return this.sendOp(OP.ERROR, { error });
   }
 
   // trap unregistered users
@@ -34,26 +61,49 @@ function clientReceiveMessage( message ){
       if( players.has(msg.payload.username) ){
         // player name is taken
         const error = `username: '${msg.payload.username}' is not available.`;
-        this.send(OP.create(OP.ERROR, { error }));
+        this.sendOp(OP.ERROR, { error });
       } else {
         // username is available, register the player
         this.username = msg.payload.username;
         players.set(this.username, this);
-        this.send(OP.create(OP.REGISTERACK));
+        this.sendOp(OP.REGISTERACK);
       }
     } else {
       const error = `You are not registered yet. Register with OP:REGISTER first.`;
-      this.send(OP.create(OP.ERROR, { error }));
+      this.sendOp(OP.ERROR, { error });
     }
     return; // trap
   }
 
+  this.clientHandleOp(msg);
+}
+
+function clientDisconnect(){
+  if( this.username !== null ){
+    if( players.has(this.username) ){
+      players.delete(this.username);
+    }
+  }
+  console.info(`Client username:'${this.username}' has disconnected.`);
+}
+
+// handles errors
+function sendOp(op, payload){
+  this.send(OP.create(op, payload), error => {
+    if( error !== undefined ){
+      console.error(`Error writing to client socket`, error);
+      clientDisconnect.call(this);
+    }
+  });
 }
 
 wss.on('connection', client => {
   client.username = null;
+  client.sendOp = sendOp;
+  client.clientHandleOp = clientHandleOp;
 
   client.on('message', clientReceiveMessage.bind(client));
+  client.on('close', clientDisconnect.bind(client));
 
 });
 
